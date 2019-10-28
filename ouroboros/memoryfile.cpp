80ad9670e3254ef6141f83a73d2d3c5e0e7ed89e
Mikhail Belyaev <belyaev_ms@mail.ru>
2019-10-28 14:28:22 +0500

    fix(memoryfile)

--- ouroboros/memoryfile.cpp
+++ ouroboros/memoryfile.cpp
@@ -127,7 +127,7 @@ void memory_file::refresh(size_type size, const pos_type pos)
 /**
  * Forced synchronization data of the file
  */
-void file::flush() const
+void memory_file::flush() const
 {
 //    OUROBOROS_THROW_BUG("method not supported");
 }
