79a64e4930b6f20f2de7773021fb4c2e649c4b65
Mikhail Belyaev <belyaev_ms@mail.ru>
2020-11-27 13:04:19 +0500

    fix(test): fixed sharedlocker_test

--- tests/sharedlocker_test.cpp
+++ tests/sharedlocker_test.cpp
@@ -1,19 +1,70 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE sharedlocker_test
 #include <boost/test/unit_test.hpp>
+#include <boost/mpl/list.hpp>
 
 #include "ouroboros/sharedlocker.h"
 
-BOOST_AUTO_TEST_CASE(simple_test)
+typedef boost::mpl::list<ouroboros::mutex_lock, ouroboros::simple_mutex_lock> lock_types;
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(simple_lock_test, lock_type, lock_types)
 {
-    ouroboros::mutex_lock locker("locktest");
+    lock_type locker;
     BOOST_REQUIRE(locker.lock());
-    BOOST_REQUIRE(!locker.lock());
-    BOOST_REQUIRE(!locker.lock_sharable());
     BOOST_REQUIRE(locker.unlock());
-    BOOST_REQUIRE(locker.lock_sharable());
-    BOOST_REQUIRE(!locker.lock());
+    BOOST_REQUIRE(locker.lock(1));
+    BOOST_REQUIRE(locker.unlock());
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(simple_lock_sharable_test, lock_type, lock_types)
+{
+    lock_type locker;
     BOOST_REQUIRE(locker.lock_sharable());
     BOOST_REQUIRE(locker.unlock_sharable());
+    BOOST_REQUIRE(locker.lock_sharable(1));
+    BOOST_REQUIRE(locker.unlock_sharable());
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(blocking_lock_test, lock_type, lock_types)
+{
+    lock_type locker;
+    BOOST_REQUIRE(locker.lock());
+    BOOST_REQUIRE(locker.unlock());
+    BOOST_REQUIRE(locker.lock_sharable());
+    BOOST_REQUIRE(locker.unlock_sharable());
+
+    BOOST_REQUIRE(locker.lock());
+    BOOST_REQUIRE(!locker.lock());
+    BOOST_REQUIRE(!locker.lock(1));
+    BOOST_REQUIRE(!locker.lock_sharable());
+    BOOST_REQUIRE(!locker.lock_sharable(1));
+    BOOST_REQUIRE(locker.unlock());
+
+    BOOST_REQUIRE(locker.lock(1));
+    BOOST_REQUIRE(locker.unlock());
+    BOOST_REQUIRE(locker.lock_sharable(1));
+    BOOST_REQUIRE(locker.unlock_sharable());
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(blocking_lock_sharable_test, lock_type, lock_types)
+{
+    lock_type locker;
+    BOOST_REQUIRE(locker.lock());
+    BOOST_REQUIRE(locker.unlock());
+    BOOST_REQUIRE(locker.lock_sharable());
+    BOOST_REQUIRE(locker.unlock_sharable());
+
+    BOOST_REQUIRE(locker.lock_sharable());
+    BOOST_REQUIRE(!locker.lock());
+    BOOST_REQUIRE(!locker.lock(1));
+    BOOST_REQUIRE(locker.lock_sharable());
+    BOOST_REQUIRE(locker.lock_sharable(1));
+    BOOST_REQUIRE(locker.unlock_sharable());
+    BOOST_REQUIRE(locker.unlock_sharable());
+    BOOST_REQUIRE(locker.unlock_sharable());
+
+    BOOST_REQUIRE(locker.lock(1));
+    BOOST_REQUIRE(locker.unlock());
+    BOOST_REQUIRE(locker.lock_sharable(1));
     BOOST_REQUIRE(locker.unlock_sharable());
 }
